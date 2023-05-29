48bd3c76e71d72d946c781d095b9ee7ca6c86c8f
shbhm96 <shbhm96@gmail.com>
2023-05-29 22:58:22 +0530

    adding new branch and adding deploy to netlify code

---
+++ routes/rootRouter.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { getProductById, getProducts } from "../controller/productController.js";
+import productRoutes from "./routes/productsRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import orderRoutes from "./routes/orderRoutes.js"
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import adminRoutes from "./routes/adminRoutes.js"
+import uploadRoutes from "./routes/uploadRoutes.js"
+
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.send("API is running....")
+})
+
+router.route("/api/test",(req,res)=>{
+    res.send("TEST SUCCESSFULL")
+})
+router.route("/api/products",productRoutes)
+router.route("/api/users",userRoutes)
+router.route("/api/orders",orderRoutes)
+router.route("/api/admin",adminRoutes)
+router.route("/api/upload",uploadRoutes)
+
+const __dirname = path.resolve()
+router.route("/uploads",express.static(path.join(__dirname,'/uploads')))
+
+app.use(notFound)
+
+app.use(errorHandler)
+
+export default router
