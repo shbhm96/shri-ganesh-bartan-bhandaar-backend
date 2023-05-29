2d3b79e426088093b6a09ee69520249bf1625d1a
shbhm96 <shbhm96@gmail.com>
2023-05-29 23:31:19 +0530

    adding deploy 2 netlify setup

---
+++ functions/server.js
@@ -0,0 +1,27 @@
+import path from "path"
+import express from 'express';
+import dotenv from "dotenv";
+import connectMongooseDB from '../config/database.js';
+import colors from 'colors';
+import rootRouter from "../routes/rootRouter.js"
+import cors from 'cors'
+import serverless from "serverless-http"
+
+dotenv.config()
+
+connectMongooseDB()
+
+const app = express()
+
+app.use("/.netlify/functions/api",rootRouter)
+
+app.use(express.json())
+
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,
+    console.log(`server running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold)
+    )
+
+export default serverless(app)
